f29484be91f6c8cf217d8ba935f878b43315453b
Siyanda Mayekiso <s.mayekiso@gmail.com>
2023-01-25 10:29:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function searchIngred(foodItem) {
   $(".results").empty();
-  var queryURL = "https://api.edamam.com/api/recipes/v2?type=public&beta=true'" + foodItem + "'&app_id=3e994090&app_key=%2068f28fd268a60e49f62d0e1eac7e46d3&random=true"
+  var queryURL = `https://api.edamam.com/api/recipes/v2?type=public&beta=true'${foodItem}'&app_id=3e994090&app_key=%2068f28fd268a60e49f62d0e1eac7e46d3&random=true`
   $.ajax({
       url: queryURL,
       method: "GET"
